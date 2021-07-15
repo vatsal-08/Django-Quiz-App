fd7cc574625c76972ffc9fa597409427e8db50de
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-07-15 09:27:41 +0530

    This project can help teachers take the test of students easily

---
+++ quiz_proj/quizes/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Quiz
+
+from django.views.generic import ListView
+
+
+class QuizListView(ListView):
+    model = Quiz
+    template_name = 'quizes/main.html'
+
+
+def quiz_view(request, pk):
+    quiz = Quiz.objects.get(pk=pk)
+    return render(request, 'quizes/quiz.html', {'obj': quiz})
