c2467e9f8dd2c8e60ba0a02935cb7b7eb5f6755c
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-07-21 12:44:14 +0530

    Basic working of quiz app is ready and it just needs timer and total score related functionality

--- quiz_proj/quizes/urls.py
+++ quiz_proj/quizes/urls.py
@@ -11,6 +11,6 @@ app_name = 'quizes'
 urlpatterns = [
     path('', QuizListView.as_view(), name='main-view'),
     path('<int:pk>/', quiz_view, name='quiz-view'),
-    path('<int:pk>/save/', quiz_data_view, name='quiz-data-view'),
-    path('<int:pk>/data/', save_quiz_view, name='save-view'),
+    path('<int:pk>/data/', quiz_data_view, name='quiz-data-view'),
+    path('<int:pk>/save/', save_quiz_view, name='save-view'),
 ]
