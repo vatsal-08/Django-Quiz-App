4248ec5763dd5f41c0d11ee3ede4f30ce61a8a81
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-07-21 16:51:27 +0530

    Basic working of quiz app is ready and it just needs timer and total score related functionality

--- quiz_proj/quizes/views.py
+++ quiz_proj/quizes/views.py
@@ -3,6 +3,8 @@ from .models import Quiz
 from questions.models import Question, Answer
 from django.views.generic import ListView
 from django.http import JsonResponse
+from questions.models import Question
+from results.models import Result
 
 
 class QuizListView(ListView):
@@ -31,5 +33,47 @@ def quiz_data_view(request, pk):
 
 
 def save_quiz_view(request, pk):
-    print(request.POST)
-    return JsonResponse({'text': 'works'})
+    # print(request.POST)
+    if request.is_ajax():
+        questions = []
+        data = request.POST
+        data_ = dict(data.lists())
+        data_.pop('csrfmiddlewaretoken')
+        for k in data_.keys():
+            print('keys: ', k)
+            question = Question.objects.get(text=k)
+            questions.append(question)
+        print(questions)
+
+        user = request.user
+        quiz = Quiz.objects.get(pk=pk)
+        score = 0
+        multiplier = 100/quiz.number_of_questions
+        results = []
+        correct_answer = None
+        for q in questions:
+            a_selected = request.POST.get(q.text)
+
+            if a_selected != "":
+                question_answers = Answer.objects.filter(question=q)
+                for a in question_answers:
+                    if a_selected == a.text:
+                        if a.correct:
+                            score += 1
+                            correct_answer = a.text
+                    else:
+                        if a.correct:
+                            correct_answer = a.text
+
+                results.append(
+                    {str(q): {'correct_answer': correct_answer, 'answered': a_selected}})
+            else:
+                results.append({str(q): 'not answered'})
+
+        score_ = score * multiplier
+        Result.objects.create(quiz=quiz, user=user, score=score_)
+
+        if score_ >= quiz.required_score_to_pass:
+            return JsonResponse({'passed': True, 'score': score_, 'results': results})
+        else:
+            return JsonResponse({'passed': False, 'score': score_, 'results': results})
