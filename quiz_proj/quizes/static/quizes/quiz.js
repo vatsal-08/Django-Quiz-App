c148219334e4ce0b044ec733eac79568ec24a55f
Vatsal Khatri <201951081@iiitvadodara.ac.in>
2021-07-22 10:52:11 +0530

    All functionalities has been added and only timer functionality is left to be added

--- quiz_proj/quizes/static/quizes/quiz.js
+++ quiz_proj/quizes/static/quizes/quiz.js
@@ -1,6 +1,8 @@
 console.log("hello world quiz");
 const url = window.location.href;
 const quizBox = document.getElementById("quiz-box");
+const scoreBox = document.getElementById("score-box");
+const resultBox = document.getElementById("result-box");
 
 $.ajax({
   type: "GET",
@@ -53,7 +55,40 @@ const sendData = () => {
     url: `${url}save/`,
     data: data,
     success: function (response) {
-      console.log(response);
+      // console.log(response);
+      const results = response.results;
+      console.log(results);
+      quizForm.classList.add("not-visible");
+
+      scoreBox.innerHTML += `${
+        response.passed ? "Congratulations" : "OOps..:("} Your result is ${response.score.toFixed(2)}%`;
+
+      results.forEach((res) => {
+        const resDiv = document.createElement("div");
+        for (const [question, resp] of Object.entries(res)) {
+          resDiv.innerHTML += question;
+          const cls = ["container", "p-3", "text-light", "h3"];
+          resDiv.classList.add(...cls);
+          if (resp == "not answered") {
+            resDiv.innerHTML += "- not answered";
+            resDiv.classList.add("bg-danger");
+          } else {
+            const answer = resp["answered"];
+            const correct = resp["correct_answer"];
+
+            if (answer == correct) {
+              resDiv.classList.add("bg-success");
+              resDiv.innerHTML += ` answered : ${answer}`;
+            } else {
+              resDiv.classList.add("bg-danger");
+              resDiv.innerHTML += ` | correct answer : ${correct}`;
+              resDiv.innerHTML += ` | answered : ${answer}`;
+            }
+          }
+        }
+        // const body = document.getElementsByTagName("BODY")[0];
+        resultBox.append(resDiv);
+      });
     },
     error: function (error) {
       console.log(error);
